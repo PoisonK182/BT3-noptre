8d7f37d8b021378643b7083c3ac4913b1976fd88
PoisonK182 <23521211@gm.uit.edu.vn>
2024-12-14 23:06:09 +0700

    BTLT3

---
+++ BaiTap/excercise-2/login.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace excercise_2
+{
+    public partial class login : Form
+    {
+        private Socket Client;
+        private string LocalIP;
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+        public static string Passdecode(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public string GetLocalIPAddress()
+        {
+            try
+            {
+                foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        return ip.ToString();
+                    }
+                }
+                throw new Exception("Không tìm thấy địa chỉ IPv4 nào!");
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        public login()
+        {
+            InitializeComponent();
+            LocalIP = GetLocalIPAddress();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            signup log = new signup();
+            log.Show();
+            this.Hide();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = Passdecode(textBox2.Text);
+            string ServerIp = LocalIP;
+            int port = 11000;
+
+            Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ServerIp), port);
+            await Task.Run(() => Client.Connect(endPoint));
+
+            var Loginpacket = new Packet("LoginRequest", "", username, "", password, "");
+            string packetString = Loginpacket.ToPacketString();
+
+
+            byte[] messageBytes = Encoding.UTF8.GetBytes(packetString);
+            await Task.Run(() => Client.Send(messageBytes));
+
+
+        }
+        
+        private void label6_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+         
+        }
+    }
+}
+
+
